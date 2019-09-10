b7ba9d1b739fb656ad00248d9692cb766daefc23
filipeamorais@pm.me <filipeamorais@pm.me>
2019-09-10 14:44:22 -0500

    SECOND_PART - add: added the part of the code for the button attribute to send message and starting to write the sendMessage function.

--- app/src/main/java/com/example/firstlayout/MainActivity.java
+++ app/src/main/java/com/example/firstlayout/MainActivity.java
@@ -11,4 +11,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(
+                this, DisplayMessageActivity.class);
+        EditText editText =
+                (EditText) findViewById(R.id.message);
+        String message = editText.getText().toString();
+        intent.putExtra(TXT, message);
+        startActivity(intent);
+
+    }
 }
